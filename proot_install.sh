0d67cd142a64c28944c245cdb215abf028f92df7
Mohd Abuzar <venomdemon718@gmail.com>
2026-05-11 08:38:59 +0530

    Update proot_install.sh

--- proot_install.sh
+++ proot_install.sh
@@ -23,22 +23,23 @@ echo -e "${YLW}Updating packages...${RST}"
 #!/bin/bash
 
 # --- Termux Level Commands ---
+pkg update && pkg upgrade -y
+pkg install proot-distro -y
 
-pkg update && apt upgrade -y
+# Install and login to Ubuntu
+proot-distro install ubuntu
 
-echo -e "${CYN} Installing Dependencies......${RST}"
+# Use proot-distro login with -- to execute commands inside Ubuntu
+proot-distro login ubuntu -- bash -c "
+    apt update && apt upgrade -y
+    apt install python3 python3-pip python3-venv git curl build-essential nodejs npm -y
 
-pkg install python python-pip git curl nodejs-lts -y
+    git clone https://github.com/NousResearch/hermes-agent.git
+    cd hermes-agent
 
-git clone https://github.com/NousResearch/hermes-agent.git
-cd hermes-agent
-
-echo -e "${CYN} Cloning Hermes Repo......${RST}"
-pip install --upgrade pip
-
-echo -e "${CYN} Installing Hermes Agent Dependencies......${RST}"
-pip install -e .
-    
+    pip install --upgrade pip
+    pip install -e .
+"
 echo -e "${CYN}===================================================${RST}"
 echo -e "${GRN}      ✅ Hermes Agent installed successfully!"
 echo -e "${CYN}===================================================${RST}"
@@ -47,5 +48,6 @@ echo "📖 Type 'hermes --help' for more options"
 echo ""
 echo "💡 Need help? Visit: https://github.com/AbuZar-Ansarii/Hermes-Agent-On-Android"
 echo ""
+
 echo -e "${CYN} 🌐 Run 'hermes gateway' to run deply it${RST}"
 echo -e "${CYN} 🔥 Run 'hermes' or 'hermes setup' to start using it ${RST}"
